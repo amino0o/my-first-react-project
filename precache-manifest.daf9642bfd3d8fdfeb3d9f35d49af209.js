fcce0fd03ab4c78b20acaadb0e2e45b2d410a2e9
Amin Golbaz <golbaz.amin@gmail.com>
2019-12-17 16:40:09 +0330

    Updates

---
+++ precache-manifest.daf9642bfd3d8fdfeb3d9f35d49af209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f52bdcbad36e4eb2db80b94f9497a8",
+    "url": "/my-first-react-project/index.html"
+  },
+  {
+    "revision": "813d02c0b42ff14f6dd3",
+    "url": "/my-first-react-project/static/css/main.fc3b0903.chunk.css"
+  },
+  {
+    "revision": "d706f28003964d78c4b2",
+    "url": "/my-first-react-project/static/js/2.1339efe9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-first-react-project/static/js/2.1339efe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "813d02c0b42ff14f6dd3",
+    "url": "/my-first-react-project/static/js/main.352b3eb4.chunk.js"
+  },
+  {
+    "revision": "69358f4d9076878f7961",
+    "url": "/my-first-react-project/static/js/runtime-main.ef17c956.js"
+  }
+]);
